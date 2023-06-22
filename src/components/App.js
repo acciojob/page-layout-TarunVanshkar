37b7915008ae93310af33b3204f5cd7a9029f88d
TarunVanshkar <tarunben33@gmail.com>
2023-06-22 20:44:01 +0530

    solve2

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,18 @@
-
 import React from "react";
 import './../styles/App.css';
-import Content from "./content";
+import PageLayout from "./PageLayout";
 
-
-const App = () => {
+function App() {
   return (
     <div>
-      {/* Do not remove the main div */}
-      <Content
+      <PageLayout
         header={<h1>Welcome to my website</h1>}
         footer={<p>© 2023 My Website. All rights reserved.</p>}
       >
         <p>This is the content of my website.</p>
-      </Content>    
+      </PageLayout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
